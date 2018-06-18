0f6681100c08e2f1bfa3920c22ce2cde40a0144f
Sujith Srivardhan Arram <suji.srivardhan@gmail.com>
2018-06-18 00:49:21 -0400

    redirecting to profile page if user already logged in when clicked on login button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Route, Router} from "@angular/router";
 import {UserServiceClient} from "../services/user.service.client";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  username;
-  password;
-  login(username, password) {
-    console.log([username, password]);
-    this.service
-      .login(username, password)
-      .then(() => {
-        this.router.navigate(['profile']);
-      });
-  }
+    username;
+    password;
 
-  constructor(private router: Router,
-              private service: UserServiceClient) { }
+    constructor(private router: Router,
+                private service: UserServiceClient) {
+        let login_check = '';
+        service.profile().then(response => {
+                login_check = response._id;
+                console.log(login_check);
+                return login_check;
+            }
+        ).then(profileId => {
+            if (profileId !== null) {
+                this.router.navigate(['profile']);
+            }
+        });
+    }
 
-  ngOnInit() {
-  }
+    login(username, password) {
+        console.log([username, password]);
+        this.service
+            .login(username, password)
+            .then(() => {
+                this.router.navigate(['profile']);
+            });
+    }
+
+    ngOnInit() {
+    }
 
 }
