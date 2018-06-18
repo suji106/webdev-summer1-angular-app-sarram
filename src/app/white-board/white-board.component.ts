083d382b29a7e60a552b60fde27cc577ca14d1f5
Sujith Srivardhan Arram <suji.srivardhan@gmail.com>
2018-06-18 02:06:14 -0400

    Displaying sections for the user if already logged in

--- src/app/white-board/white-board.component.ts
+++ src/app/white-board/white-board.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserServiceClient} from "../services/user.service.client";
+import {Router} from "@angular/router";
+import {SectionServiceClient} from "../services/section.service.client";
 
 @Component({
-  selector: 'app-white-board',
-  templateUrl: './white-board.component.html',
-  styleUrls: ['./white-board.component.css']
+    selector: 'app-white-board',
+    templateUrl: './white-board.component.html',
+    styleUrls: ['./white-board.component.css']
 })
+
 export class WhiteBoardComponent implements OnInit {
 
-  constructor() { }
+    sections = [];
+    username = '';
 
-  ngOnInit() {
-  }
+    constructor(private router: Router,
+                private service: UserServiceClient,
+                private sectionService: SectionServiceClient) {
+    }
+
+    logout() {
+        this.service
+            .logout()
+            .then(() =>
+                this.router.navigate(['login']));
+    }
+
+    ngOnInit() {
+        this.service
+            .profile()
+            .then(user =>
+                this.username = user.username);
+        this.sectionService
+            .findSectionsForStudent()
+            .then(sections => this.sections = sections);
+    }
 
 }
