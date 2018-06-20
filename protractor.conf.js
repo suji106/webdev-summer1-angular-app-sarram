32976aec8f9a9848c96b78e05fa366a7e1606db2
Sujith Srivardhan Arram <suji.srivardhan@gmail.com>
2018-06-20 02:45:36 -0400

    update to support heroku deploy

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 's-arram-angular.herokuapp.com/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
