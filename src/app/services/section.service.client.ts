f4125911e5719e4050a0f60356efa2f251d46018
Sujith Srivardhan Arram <suji.srivardhan@gmail.com>
2018-06-20 21:26:54 -0400

    changing url to heroku for backend

--- src/app/services/section.service.client.ts
+++ src/app/services/section.service.client.ts
@@ -1,9 +1,9 @@
 export class SectionServiceClient {
 
-    SECTION_URL = 'http://localhost:4000/api/course/COURSEID/section';
+    SECTION_URL = 'http://https://s-arram-angular.herokuapp.com/api/course/COURSEID/section';
 
     findSectionsForStudent() {
-        const url = 'http://localhost:4000/api/student/section';
+        const url = 'http://https://s-arram-angular.herokuapp.com/api/student/section';
         return fetch(url, {
             credentials: 'include'
         })
@@ -11,7 +11,7 @@ export class SectionServiceClient {
     }
 
     enrollStudentInSection(sectionId) {
-        const url = 'http://localhost:4000/api/section/' + sectionId + '/enrollment';
+        const url = 'http://https://s-arram-angular.herokuapp.com/api/section/' + sectionId + '/enrollment';
         return fetch(url, {
             method: 'post',
             credentials: 'include'
@@ -19,7 +19,7 @@ export class SectionServiceClient {
     }
 
     unrollStudentInSection(sectionId) {
-        const url = 'http://localhost:4000/api/section/' + sectionId + '/enrollment';
+        const url = 'http://https://s-arram-angular.herokuapp.com/api/section/' + sectionId + '/enrollment';
         return fetch(url, {
             method: 'delete',
             credentials: 'include'
@@ -29,7 +29,7 @@ export class SectionServiceClient {
     updateSection(sectionId, newName, newMax, newRem) {
         const section = {id: sectionId, newName: newName, newMax: newMax, newRem: newRem};
         console.log(section);
-        return fetch('http://localhost:4000/api/section' + '/' + sectionId, {
+        return fetch('http://https://s-arram-angular.herokuapp.com/api/section' + '/' + sectionId, {
             method: 'put',
             body: JSON.stringify(section),
             credentials: 'include',
@@ -40,7 +40,7 @@ export class SectionServiceClient {
     }
 
     deleteSection(sectionId) {
-        return fetch('http://localhost:4000/api/section' + '/' + sectionId, {
+        return fetch('http://https://s-arram-angular.herokuapp.com/api/section' + '/' + sectionId, {
             method: 'delete'
         });
     }
